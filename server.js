f4408483e1d2a454b7069212c0b81325d51bd65f
dbae82 <dbae82@gmail.com>
2022-03-02 00:08:31 -0600

    build: init files created with server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const PORT = 4000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
