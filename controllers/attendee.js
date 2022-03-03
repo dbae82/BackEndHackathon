c17623eb1fefe829ad450c9766dbd11a21753244
dbae82 <dbae82@gmail.com>
2022-03-03 12:34:03 -0600

    edit: cleaned up code

--- controllers/attendee.js
+++ controllers/attendee.js
@@ -15,8 +15,8 @@ const index = (req, res) => {
 const team = (req, res) => {
     db.Attendee.find({ team: req.query.q }, (error, foundAttendees) => {
         if (error) {
-            console.log('Error in attendees#index:', error)
-            return res.send('Incomplete attendees#index controller function')
+            console.log('Error in attendees#team:', error)
+            return res.send('Incomplete attendees#team controller function')
         }
         res.status(200).json({
             attendees: foundAttendees,
@@ -27,8 +27,8 @@ const team = (req, res) => {
 const company = (req, res) => {
     db.Attendee.find({ Company: req.query.q }, (error, foundAttendees) => {
         if (error) {
-            console.log('Error in attendees#index:', error)
-            return res.send('Incomplete attendees#index controller function')
+            console.log('Error in attendees#company:', error)
+            return res.send('Incomplete attendees#company controller function')
         }
         res.status(200).json({
             attendees: foundAttendees,
@@ -39,8 +39,8 @@ const company = (req, res) => {
 const title = (req, res) => {
     db.Attendee.find({ title: req.query.q }, (error, foundAttendees) => {
         if (error) {
-            console.log('Error in attendees#index:', error)
-            return res.send('Incomplete attendees#index controller function')
+            console.log('Error in attendees#title:', error)
+            return res.send('Incomplete attendees#title controller function')
         }
         res.status(200).json({
             attendees: foundAttendees,
