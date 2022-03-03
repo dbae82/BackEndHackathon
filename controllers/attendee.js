bb71677f56532fdd552e0cb3fd6afc82b77545fb
dbae82 <dbae82@gmail.com>
2022-03-03 12:28:03 -0600

    feat: all routes created with finding attendees via queries

--- controllers/attendee.js
+++ controllers/attendee.js
@@ -12,6 +12,45 @@ const index = (req, res) => {
     })
 }
 
+const team = (req, res) => {
+    db.Attendee.find({ team: req.query.q }, (error, foundAttendees) => {
+        if (error) {
+            console.log('Error in attendees#index:', error)
+            return res.send('Incomplete attendees#index controller function')
+        }
+        res.status(200).json({
+            attendees: foundAttendees,
+        })
+    })
+}
+
+const company = (req, res) => {
+    db.Attendee.find({ Company: req.query.q }, (error, foundAttendees) => {
+        if (error) {
+            console.log('Error in attendees#index:', error)
+            return res.send('Incomplete attendees#index controller function')
+        }
+        res.status(200).json({
+            attendees: foundAttendees,
+        })
+    })
+}
+
+const title = (req, res) => {
+    db.Attendee.find({ title: req.query.q }, (error, foundAttendees) => {
+        if (error) {
+            console.log('Error in attendees#index:', error)
+            return res.send('Incomplete attendees#index controller function')
+        }
+        res.status(200).json({
+            attendees: foundAttendees,
+        })
+    })
+}
+
 module.exports = {
     index,
+    team,
+    company,
+    title,
 }
--- routes/attendee.js
+++ routes/attendee.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const ctrl = require('../controllers')
 
 router.get('/', ctrl.attendee.index)
+router.get('/team', ctrl.attendee.team)
+router.get('/company', ctrl.attendee.company)
+router.get('/title', ctrl.attendee.title)
 
 module.exports = router
